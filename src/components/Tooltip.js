9fe42ec0f35d1980c8047b09a4da3ffb8fc77273
VENKATA_REDDY <venkatareddypadala1637@gmail.com>
2025-12-04 18:47:35 +0530

    first commit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -6,28 +6,12 @@ function Tooltip({ text, children }) {
   return (
     <div
       className="tooltip"
-      style={{ position: "relative" }}
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
     >
       {children}
 
-      {visible && (
-        <span
-          style={{
-            color: "white",
-            backgroundColor: "red",
-            position: "absolute",
-            left: "20px",
-            bottom: "30px",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-          className="tooltiptext"
-        >
-          {text}
-        </span>
-      )}
+      {visible && <div className="tooltiptext">{text}</div>}
     </div>
   );
 }
